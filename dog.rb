282480c7441f9c48272a87a04eb869f42cab5c0e
Petros Amiridis <petros@github.com>
2016-09-23 13:25:49 +0300

    Add dog ruby class

---
+++ dog.rb
@@ -0,0 +1,15 @@
+class Dog  
+  def initialize(breed, name)  
+    # Instance variables  
+    @breed = breed  
+    @name = name  
+  end
+  
+  def bark
+    puts 'Ruff! Ruff!'
+  end
+  
+  def display
+    puts "I am of #{@breed} breed and my name is #{@name}"
+  end  
+end
